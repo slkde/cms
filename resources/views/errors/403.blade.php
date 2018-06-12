a5eaf697b6770a84cfa553ceb4caca04b4a1b1c7
Arcueid Brunestud <slkde@sina.com>
2018-06-12 13:30:25 +0800

    up

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,48 @@
+@extends('home.layouts.master')
+
+@section('title', '集安信息网')
+
+@section('content')
+
+@push('css')
+<link rel="stylesheet" href="/static/home/bootstrap-select/css/bootstrap-select.min.css">
+<style type="text/css">
+	html {
+    	overflow-y:scroll;
+	}
+</style>
+@endpush
+
+@push('js')
+<script type="text/javascript" src="/static/home/bootstrap-select/js/bootstrap-select.min.js"> </script>
+<script type="text/javascript" src="/static/home/bootstrap-filestyle/src/bootstrap-filestyle.min.js"> </script>
+<script type="text/javascript">
+// $(document).ready(function(){
+// 	$("#captchaImage").click(function(){
+// 		this.src='/captcha?d='+Math.random();
+// 	});
+// });
+</script>
+@endpush
+<ol class="breadcrumb small">
+  <li><a href="/">首页</a></li>
+  <li class="active">提示信息</li>
+</ol>
+<div class="row">
+  <div  class="col-md-12">
+		<div class="jumbotron text-center">
+				<br />
+				<h4>禁止访问</h4>
+			  <h4>{{ Session::get('message') }}</h4>
+		  <br />
+		  <p>
+			  <a href="/post" class="btn btn-default" role="button">&nbsp;&nbsp;发&nbsp;布&nbsp;信&nbsp;息&nbsp;&nbsp;</a>
+			  
+			  <a href="/" class="btn btn-default" role="button">&nbsp;&nbsp;返&nbsp;回&nbsp;首&nbsp;页&nbsp;&nbsp;</a>			  
+		  </p>
+
+		</div>
+    </div>
+</div>
+
+@endsection
