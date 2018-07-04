7481fbc9430efeb0036922fccd3b9215a9941a24
Arcueid Brunestud <slkde@sina.com>
2018-07-04 17:05:58 +0800

    up

--- app/Http/Controllers/Home/HomeController.php
+++ app/Http/Controllers/Home/HomeController.php
@@ -20,15 +20,17 @@ class HomeController extends Controller
         // 判断是否为手机用户
         $userAgent  = new Agent();
         $isMobile = $userAgent->isMobile() ? 'Y': 'N';
-        // 获取所有信息
+        // 获取信息分页
         $latestItems = Cache::remember('home_latest_items', 30, function(){
             return Article::select(DB::raw('id,category_id,district_id,is_mobile,title,created_at, (ISNULL(index_top_expired) || index_top_expired < now()) AS p'))->where('is_verify', '=', 'Y')->orderBy('p', 'asc')->latest('created_at')->Paginate(50);
         });
         // 获取所有评论
         $comments = Cache::remember('home_latest_comments', 1440, function () {
-            return Comment::select('id','content')->where('is_verify', '=', 'Y')->latest('created_at')->Paginate(50);
+            return Comment::select('id','article_id','content')->where('is_verify', '=', 'Y')->latest('created_at')->Paginate(50);
         });
-        $blog_articles = DB::connection('blog')->select("SELECT id,post_title FROM `wp_posts` WHERE post_status=\"publish\" AND post_type=\"post\" ORDER BY id DESC");
+        $blog_articles = Cache::remember('home_blog', 1440, function () {
+            return DB::connection('blog')->select("SELECT id,post_title FROM `wp_posts` WHERE post_status=\"publish\" AND post_type=\"post\" ORDER BY id DESC");
+        });
         // $menu = Category::where('pid', 0)->get();
         return view('home.index.index', compact('latestItems', 'comments', 'isMobile', 'blog_articles'));
     }
